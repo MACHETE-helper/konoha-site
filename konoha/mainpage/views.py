f31f83c94cc311b368e2facb9666899c4f2d7685
MACHETE-helper <OnlyMamlya@gmail.com>
2024-02-09 14:40:10 +0200

    test pre alpha version 1.0

--- konoha/mainpage/views.py
+++ konoha/mainpage/views.py
@@ -10,10 +10,10 @@ def about(request):
     return render(request, 'mainpage/about.html', {})
 
 def price(request):
-    return HttpResponse("<h4>Проверка работы. Цены</h4>")
+    return render(request, 'mainpage/price.html', {})
 
 def hokage(request):
     return HttpResponse("<h4>Проверка работы. Обо мне</h4>")
 
 def teachers(request):
-    return HttpResponse("<h4>Проверка работы. Учителя</h4>")
+    return render(request, 'mainpage/teachers.html', {})
