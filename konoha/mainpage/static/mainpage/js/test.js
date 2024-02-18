df6fc412c40f7912a14c815466082479fa4de66f
MACHETE-helper <OnlyMamlya@gmail.com>
2024-02-18 19:38:09 +0200

    test pre alpha version 1.0

--- konoha/mainpage/static/mainpage/js/test.js
+++ konoha/mainpage/static/mainpage/js/test.js
@@ -2,4 +2,33 @@ $('.menu-btn').on('click', function (e){
   e.preventDefault();
   $('.menu').toggleClass('menu_active');
   $('.content').toggleClass('content_active');
+  $('.content').fadeIn('slow')
 })
+
+let btn = document.querySelector('.teacher_name_ana');
+let mytxt = document.querySelector('.teachers_txt');
+let dropcheck = true;
+
+//btn.addEventListener('click', () => {
+//  mytxt.classList.toggle('show');
+//});
+
+function showTextAna() {
+  let textElement = document.getElementById('teachers_txt');
+  textElement.classList.toggle('show');
+}
+
+function hideText() {
+  let textElement = document.getElementById('teachers_txt');
+  let computedStyle = window.getComputedStyle(textElement);
+  let displayValue = computedStyle.getPropertyValue('display');
+  console.log(displayValue);
+  if (displayValue == 'block') {
+    textElement.classList.toggle('hidetxt');
+  }
+
+}
+
+
+
+
